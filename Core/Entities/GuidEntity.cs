17e93b5416f0ba727dca3499c4e428ba75984aed
Ryan Blackman <rblackman@mtd.org>
2024-11-18 14:30:34 -0600

    remove the SetRequiredMembers attribure

--- Core/Entities/GuidEntity.cs
+++ Core/Entities/GuidEntity.cs
@@ -1,17 +1,9 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace Mtd.Core.Entities
 {
-	public abstract class GuidEntity : Entity, IIdentity<string>
+	public abstract class GuidEntity() : Entity, IIdentity<string>
 	{
 		private const string GUID_FORMAT = "N";
-		public required string Id { get; set; }
-
-		[SetsRequiredMembers()]
-		protected GuidEntity()
-		{
-			Id = Guid.NewGuid().ToString(GUID_FORMAT);
-		}
+		public required string Id { get; set; } = Guid.NewGuid().ToString(GUID_FORMAT);
 
 		public static string FormatGuid(Guid guid) => guid.ToString(GUID_FORMAT);
 	}
