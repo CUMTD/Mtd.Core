8f7681793fc9bbc641505e1add7d9e310b18fd24
Ryan Blackman <rblackman@mtd.org>
2024-12-02 16:35:50 -0600

    remove required modifier (#8)

--- Core/Entities/GuidEntity.cs
+++ Core/Entities/GuidEntity.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mtd.Core.Entities
 {
 	public abstract class GuidEntity() : Entity, IIdentity<string>
 	{
 		private const string GUID_FORMAT = "N";
-		public required string Id { get; set; } = Guid.NewGuid().ToString(GUID_FORMAT);
+
+		[Key]
+		public string Id { get; set; } = Guid.NewGuid().ToString(GUID_FORMAT);
 
 		public static string FormatGuid(Guid guid) => guid.ToString(GUID_FORMAT);
 	}
