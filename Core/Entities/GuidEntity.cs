e318d88dc5f5c2598712b7743807eff42855749a
Ryan Blackman <rblackman@mtd.org>
2024-12-02 16:38:15 -0600

    switch to GUIDv7 (#9)

--- Core/Entities/GuidEntity.cs
+++ Core/Entities/GuidEntity.cs
@@ -7,7 +7,7 @@ namespace Mtd.Core.Entities
 		private const string GUID_FORMAT = "N";
 
 		[Key]
-		public string Id { get; set; } = Guid.NewGuid().ToString(GUID_FORMAT);
+		public string Id { get; set; } = Guid.CreateVersion7().ToString(GUID_FORMAT);
 
 		public static string FormatGuid(Guid guid) => guid.ToString(GUID_FORMAT);
 	}
